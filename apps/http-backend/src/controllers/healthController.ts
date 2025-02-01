0b11db2bf875c965926fd867c90d22644752373b
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-01 22:59:34 +0530

    feat: Updated Router

--- apps/http-backend/src/controllers/healthController.ts
+++ apps/http-backend/src/controllers/healthController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
-import asyncErrorHandler from "../utils/asyncErrorHandler";
-import httpResponse from "../utils/httpResponse";
-import { EResponseStatusCode } from "../constants/application";
+import { asyncErrorHandler, httpResponse } from "@repo/shared-utils";
 import quicker from "../utils/quicker";
 import dayjs from "dayjs";
+import { createToken } from "@repo/auth";
+import { StatusCodes } from "@repo/types";
 
 export default {
   self: asyncErrorHandler(async (req: Request, res: Response) => {
-    httpResponse(req, res, EResponseStatusCode.OK, "Hello World", {
+    httpResponse(req, res, StatusCodes.SUCCESS.OK, "Hello World", {
       name: "Mayank Tiwari",
     });
   }),
@@ -18,6 +18,15 @@ export default {
       system: quicker.getSystemHealth(),
       time: dayjs().format("YYYY-MM-DD HH:mm:ss"),
     };
-    httpResponse(req, res, EResponseStatusCode.OK, "Health Check", healthData);
+    httpResponse(req, res, StatusCodes.SUCCESS.OK, "Health Check", healthData);
+  }),
+
+  generateToken: asyncErrorHandler(async (req: Request, res: Response) => {
+    const user = {
+      userId: "mayank",
+      email: "Hello",
+    };
+    const token = createToken(user);
+    httpResponse(req, res, StatusCodes.SUCCESS.OK, "Health Check", token);
   }),
 };
