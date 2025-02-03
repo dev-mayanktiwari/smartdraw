6bec7875cf984be37df9e13c6e9520f8a8bc0a52
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-03 10:45:56 +0530

    feat: Added passport strategy

---
+++ apps/http-backend/src/utils/passportGoogleStrategy.ts
@@ -0,0 +1,47 @@
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { AppConfig } from "../config";
+import { prisma } from "@repo/db";
+
+const PassportGoogleStrategy = new GoogleStrategy(
+  {
+    clientID: AppConfig.get("GOOGLE_CLIENT_ID") as string,
+    clientSecret: AppConfig.get("GOOGLE_CLIENT_SECRET") as string,
+    callbackURL: "http://localhost:4000/api/v1/auth/google/callback",
+  },
+  async (_, __, profile, done) => {
+    try {
+      const email = profile._json?.email;
+
+      if (!email) {
+        console.log("Error in passport");
+        return done(new Error("No email found from google"));
+      }
+
+      // console.log(profile);
+
+      const user = await prisma.user.upsert({
+        where: {
+          email,
+        },
+        update: {
+          lastLoginAt: new Date(),
+        },
+        create: {
+          email,
+          name: profile.displayName,
+          avatar: profile.photos?.[0]?.value,
+          authProvider: "GOOGLE",
+          providerId: profile.id,
+          isVerified: true,
+        },
+      });
+
+      return done(null, user);
+    } catch (error) {
+      console.log("Error in passport:", error);
+      return done(error);
+    }
+  }
+);
+
+export default PassportGoogleStrategy;
