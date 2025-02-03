5ece0dac48ee8334e41ff215c67baa69430a327c
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-03 10:46:40 +0530

    feat: Updated health routes

--- apps/http-backend/src/routes/healthRoutes.ts
+++ apps/http-backend/src/routes/healthRoutes.ts
@@ -1,16 +1,9 @@
 import { Router } from "express";
-import { httpAuthMiddleware } from "@repo/auth";
 import healthController from "../controllers/healthController";
 
 const healthRouter: Router = Router();
 
 healthRouter.get("/self", healthController.self);
 healthRouter.get("/health", healthController.health);
-healthRouter.get("/generateToken", healthController.generateToken)
-healthRouter.get(
-  "/protectedRoute",
-  httpAuthMiddleware(),
-  healthController.self
-);
 
 export default healthRouter;
