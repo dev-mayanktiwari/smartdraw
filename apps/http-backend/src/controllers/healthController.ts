7e0a99636063347b39e46fc77c6fce45d66de6a4
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-03 10:45:35 +0530

    fix: HealthController

--- apps/http-backend/src/controllers/healthController.ts
+++ apps/http-backend/src/controllers/healthController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import { asyncErrorHandler, httpResponse } from "@repo/shared-utils";
 import quicker from "../utils/quicker";
 import dayjs from "dayjs";
-import { createToken } from "@repo/auth";
 import { StatusCodes } from "@repo/types";
 
 export default {
@@ -19,14 +18,5 @@ export default {
       time: dayjs().format("YYYY-MM-DD HH:mm:ss"),
     };
     httpResponse(req, res, StatusCodes.SUCCESS.OK, "Health Check", healthData);
-  }),
-
-  generateToken: asyncErrorHandler(async (req: Request, res: Response) => {
-    const user = {
-      userId: "mayank",
-      email: "Hello",
-    };
-    const token = createToken(user);
-    httpResponse(req, res, StatusCodes.SUCCESS.OK, "Health Check", token);
   }),
 };
