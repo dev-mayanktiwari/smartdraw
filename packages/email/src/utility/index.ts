e655b856d1ff5b2a4eeeb8a37c8c2cd8a0066d6c
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-04 23:50:05 +0530

    feat: Added email package

---
+++ packages/email/src/utility/index.ts
@@ -0,0 +1,5 @@
+export * from "./sendWelcomeEmailNodemailer";
+export * from "./sendAccountConfirmationEmailNodemailer";
+export * from "./sendEmailVerificationMailNodemailer";
+export * from "./sendPasswordChangedConfirmationMailNodemailer";
+export * from "./sendPasswordResetMailNodemailer";
