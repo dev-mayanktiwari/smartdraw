5948419c490320dcca5cd895d900811c03ff2f3e
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-05 17:28:19 +0530

    fix: Error handler

--- apps/http-backend/src/middlewares/globalErrorHandler.ts
+++ apps/http-backend/src/middlewares/globalErrorHandler.ts
@@ -8,8 +8,5 @@ export default (
   __: NextFunction
 ) => {
   console.log("Error in global error handler", err);
-  res.status(err.statusCode || 500).json({
-    message: err.message || "Internal server error",
-    statusCode: err.statusCode || 500,
-  });
+  res.status(err.statusCode || 500).json(err);
 };
