ba71125dd54404dda5c38ac2728946ff6b5903a0
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-05 17:31:25 +0530

    feat: Updated nodemailer config

--- packages/email/src/config/nodemailerConfig.ts
+++ packages/email/src/config/nodemailerConfig.ts
@@ -1,9 +1,14 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const PORT = Number(process.env.EMAIL_PORT);
-console.log(PORT);
+// console.log("Host", process.env.EMAIL_HOST);
+// console.log("User", process.env.EMAIL_USER);
+// console.log("Pass", process.env.EMAIL_PASS);
+// console.log("Port", PORT);
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
