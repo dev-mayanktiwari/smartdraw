421bbf7a96abd415eff4731c2e22165cde3a16e1
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-05 17:28:06 +0530

    feat: Updated authcontroller

--- apps/http-backend/src/controllers/authController.ts
+++ apps/http-backend/src/controllers/authController.ts
@@ -2,6 +2,10 @@ import { prisma } from "@repo/db";
 import { asyncErrorHandler, httpError, httpResponse } from "@repo/shared-utils";
 import {
   ApplicationEnvirontment,
+  AuthenticatedRequest,
+  ChangePasswordSchema,
+  ForgotUserSchema,
+  ResetPasswordSchema,
   ResponseMessage,
   StatusCodes,
   UserLoginInput,
@@ -10,15 +14,28 @@ import {
 import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
 import { AppConfig } from "../config";
-import quicker from "../utils/quicker"; 
+import quicker from "../utils/quicker";
 import { TokenService } from "@repo/auth";
 import {
-  findUserByVerificationCode,
+  addResetTokenAndExpiry,
+  clearResetTokenAndExpiry,
+  findUserByEmail,
+  findUserByIdWithouthPassword,
+  findUserByResetToken,
+  findUserByUserID,
+  findUserByVerificationCodeAndToken,
   updateAccountVerificationStatus,
   updateLastLogin,
+  updatePassword,
   updateRefreshToken,
   upsertRefreshToken,
 } from "../services/userDbServices";
+import {
+  sendPasswordResetEmail,
+  sendVerificationEmail,
+  sendWelcomeEmail,
+} from "@repo/queue";
+import dayjs from "dayjs";
 
 export default {
   register: asyncErrorHandler(
@@ -58,7 +75,9 @@ export default {
         password,
         Number(AppConfig.get("SALTROUNDS"))
       );
+
       const verificationToken = quicker.generateVerifyToken();
+      const code = quicker.generateCode(6);
 
       const newUser = await prisma.user.create({
         data: {
@@ -69,12 +88,13 @@ export default {
           Auth: {
             create: {
               verifyToken: verificationToken,
+              code: code,
             },
           },
         },
       });
 
-      // TODO: SEND EMAIL
+      await sendVerificationEmail(email, name, verificationToken, String(code));
 
       return httpResponse(
         req,
@@ -92,8 +112,9 @@ export default {
   verifyEmail: asyncErrorHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       const verifyToken = req.query.token;
+      const code = req.query.code;
 
-      if (!verifyToken) {
+      if (!verifyToken || !code) {
         httpError(
           next,
           new Error("No token provided"),
@@ -102,14 +123,32 @@ export default {
         );
       }
 
-      const user = await findUserByVerificationCode(verifyToken as string);
+      const auth = await findUserByVerificationCodeAndToken(
+        verifyToken as string,
+        Number(code)
+      );
 
-      if (!user) {
+      if (!auth) {
         httpError(
           next,
           new Error(ResponseMessage.NOT_FOUND),
           req,
           StatusCodes.ERROR.CLIENT_ERROR.UNAUTHORIZED
+        );
+      }
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: auth?.userId,
+        },
+      });
+
+      if (user?.isVerified) {
+        return httpResponse(
+          req,
+          res,
+          StatusCodes.SUCCESS.OK,
+          "Account already verified"
         );
       }
 
@@ -122,7 +161,8 @@ export default {
       //     );
       //   }
 
-      await updateAccountVerificationStatus(user?.userId!);
+      await updateAccountVerificationStatus(auth?.userId!);
+      await sendWelcomeEmail(user?.email!, user?.name!);
 
       return httpResponse(
         req,
@@ -153,6 +193,7 @@ export default {
       const existingUser = await prisma.user.findUnique({
         where: {
           email: email,
+          isActive: true,
         },
       });
 
@@ -201,6 +242,8 @@ export default {
       const __ = await updateRefreshToken(existingUser.id, refreshToken);
 
       res.cookie("refreshToken", refreshToken, {
+        path: "/api/v1/",
+        domain: String(AppConfig.get("COOKIE_DOMAIN")),
         httpOnly: true,
         secure: AppConfig.get("ENV") === ApplicationEnvirontment.PRODUCTION,
         sameSite: "lax",
@@ -219,9 +262,292 @@ export default {
     }
   ),
 
+  forgotPassword: asyncErrorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { body } = req;
+      const safeParse = ForgotUserSchema.safeParse(body);
+
+      if (!safeParse.success) {
+        // const formatted = safeParse.error.format();
+        return httpError(
+          next,
+          new Error(ResponseMessage.INVALID_INPUT),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const user = await findUserByEmail(safeParse.data.email);
+
+      if (!user) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.NOT_FOUND),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.NOT_FOUND
+        );
+      }
+
+      const expiryTime = await quicker.generateExpiryTime(15);
+      const token = quicker.generateVerifyToken();
+      await addResetTokenAndExpiry(user?.id!, token, expiryTime);
+
+      await sendPasswordResetEmail(user?.email!, user?.name!, token);
+
+      return httpResponse(
+        req,
+        res,
+        StatusCodes.SUCCESS.OK,
+        "Reset link sent to your email"
+      );
+    }
+  ),
+
+  resetPassword: asyncErrorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { body } = req;
+      const { token } = req.query;
+
+      const safeParse = ResetPasswordSchema.safeParse(body);
+
+      if (!safeParse.success) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.INVALID_INPUT),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const tokenData = await findUserByResetToken(token as string);
+      if (!tokenData) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.NOT_FOUND),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.NOT_FOUND
+        );
+      }
+
+      const user = await findUserByUserID(tokenData.userId);
+
+      if (!user?.isVerified || !user?.isActive) {
+        return httpError(
+          next,
+          new Error("User not verified or account deactivated"),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.FORBIDDEN
+        );
+      }
+
+      const initExpiryTime = dayjs(tokenData.expiryTime);
+      const currentTime = dayjs();
+      const diff = currentTime.diff(initExpiryTime, "minute");
+
+      if (diff > 15) {
+        return httpError(
+          next,
+          new Error("Reset link expired"),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.UNAUTHORIZED
+        );
+      }
+
+      const currentPassword = user.password;
+      const arepasswordSame = await bcrypt.compare(
+        safeParse.data.newPassword,
+        currentPassword!
+      );
+
+      if (arepasswordSame) {
+        return httpError(
+          next,
+          new Error("New password cannot be same as previous password"),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const hashedPassword = await bcrypt.hash(
+        safeParse.data.newPassword,
+        Number(AppConfig.get("SALTROUNDS"))
+      );
+
+      await updatePassword(user.id!, hashedPassword);
+      await clearResetTokenAndExpiry(user.id!);
+
+      return httpResponse(
+        req,
+        res,
+        StatusCodes.SUCCESS.OK,
+        "Password reset successfully"
+      );
+    }
+  ),
+
+  changePassword: asyncErrorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { body, user } = req as AuthenticatedRequest;
+
+      const safeParse = ChangePasswordSchema.safeParse(body);
+      if (!safeParse) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.INVALID_INPUT),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const dbUser = await findUserByUserID(user?.userId!);
+      if (!dbUser) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.NOT_FOUND),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.NOT_FOUND
+        );
+      }
+
+      const isPasswordCorrect = await bcrypt.compare(
+        safeParse.data?.currentPassword!,
+        dbUser.password!
+      );
+
+      if (!isPasswordCorrect) {
+        return httpError(
+          next,
+          new Error("Current password is incorrect"),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.FORBIDDEN
+        );
+      }
+
+      const arepasswordSame = await bcrypt.compare(
+        safeParse.data?.newPassword!,
+        dbUser.password!
+      );
+
+      if (arepasswordSame) {
+        return httpError(
+          next,
+          new Error("New password cannot be same as previous password"),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const hashedPassword = await bcrypt.hash(
+        safeParse.data?.newPassword!,
+        Number(AppConfig.get("SALTROUNDS"))
+      );
+
+      await updatePassword(user?.userId!, hashedPassword);
+
+      return httpResponse(
+        req,
+        res,
+        StatusCodes.SUCCESS.OK,
+        "Password changed successfully"
+      );
+    }
+  ),
+
+  logout: asyncErrorHandler(async (req: Request, res: Response) => {
+    const { cookies, user } = req as AuthenticatedRequest;
+    const { refreshToken } = cookies as { refreshToken: string | undefined };
+    const clearAuthCookies = () => {
+      const cookieOptions = {
+        path: "/api/v1/",
+        domain: String(AppConfig.get("COOKIE_DOMAIN")),
+        sameSite: "lax" as const,
+        httpOnly: true,
+        secure: AppConfig.get("ENV") === ApplicationEnvirontment.PRODUCTION,
+      };
+
+      res.clearCookie("refreshToken", cookieOptions);
+    };
+
+    if (refreshToken) {
+      await updateRefreshToken(user?.userId!, null);
+    }
+
+    clearAuthCookies();
+
+    return httpResponse(
+      req,
+      res,
+      StatusCodes.SUCCESS.OK,
+      ResponseMessage.LOGOUT_SUCCESS
+    );
+  }),
+
+  refreshToken: asyncErrorHandler(async (req: Request, res: Response) => {
+    const { cookies } = req;
+    const { refreshToken } = cookies as { refreshToken: string | undefined };
+
+    if (!refreshToken) {
+      return httpResponse(
+        req,
+        res,
+        StatusCodes.ERROR.CLIENT_ERROR.UNAUTHORIZED,
+        ResponseMessage.UNAUTHORIZED
+      );
+    }
+
+    const payload = TokenService.verifyToken(refreshToken);
+
+    if (!payload) {
+      return httpResponse(
+        req,
+        res,
+        StatusCodes.ERROR.CLIENT_ERROR.UNAUTHORIZED,
+        ResponseMessage.UNAUTHORIZED
+      );
+    }
+
+    const user = await findUserByUserID(payload.userId);
+
+    if (!user) {
+      return httpResponse(
+        req,
+        res,
+        StatusCodes.ERROR.CLIENT_ERROR.UNAUTHORIZED,
+        ResponseMessage.UNAUTHORIZED
+      );
+    }
+
+    const accessToken = TokenService.generateAccessToken({
+      userId: user.id,
+      email: user.email,
+      name: user.name,
+    });
+
+    return httpResponse(
+      req,
+      res,
+      StatusCodes.SUCCESS.OK,
+      "Token refreshed successfully",
+      {
+        accessToken: `Bearer ${accessToken}`,
+      }
+    );
+  }),
+
+  self: asyncErrorHandler(async (req: Request, res: Response) => {
+    const { user } = req as AuthenticatedRequest;
+    const dbUser = await findUserByIdWithouthPassword(user?.userId!);
+
+    return httpResponse(req, res, StatusCodes.SUCCESS.OK, "User data", {
+      dbUser,
+    });
+  }),
+
   handleGoogleCallback: asyncErrorHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       const user = req.user as any;
+
+      // console.log(user);
 
       const { accessToken, refreshToken } = TokenService.generateTokens({
         userId: user?.id,
@@ -230,9 +556,21 @@ export default {
       });
 
       await upsertRefreshToken(user?.id, refreshToken);
-      await updateLastLogin(user?.id);
+
+      // console.log("User from callback", user);
+
+      const isWelcomeEmailNeeded =
+        user.createdAt.getTime() === user.updatedAt.getTime();
+      // console.log(isWelcomeEmailNeeded);
+
+      if (isWelcomeEmailNeeded) {
+        console.log("Sending welcome email");
+        await sendWelcomeEmail(user?.email, user?.name);
+      }
 
       res.cookie("refreshToken", refreshToken, {
+        path: "/api/v1/",
+        domain: AppConfig.get("COOKIE_DOMAIN") as string,
         httpOnly: true,
         secure: AppConfig.get("ENV") === ApplicationEnvirontment.PRODUCTION,
         sameSite: "lax",
