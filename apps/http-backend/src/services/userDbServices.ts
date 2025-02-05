60d9218dc5df79890b50871a1934bf26a044b075
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-05 17:28:40 +0530

    feat: Updated db services

--- apps/http-backend/src/services/userDbServices.ts
+++ apps/http-backend/src/services/userDbServices.ts
@@ -1,5 +1,32 @@
 import { prisma, type User, type Auth } from "@repo/db";
 
+export const findUserByEmail = async (email: string): Promise<User | null> => {
+  return prisma.user.findUnique({
+    where: { email },
+  });
+};
+
+export const addResetTokenAndExpiry = async (
+  userId: string,
+  token: string,
+  expiry: string
+) => {
+  return prisma.auth.upsert({
+    where: {
+      userId,
+    },
+    create: {
+      userId,
+      verifyToken: token,
+      expiryTime: expiry,
+    },
+    update: {
+      verifyToken: token,
+      expiryTime: expiry,
+    },
+  });
+};
+
 export const updateLastLogin = async (userId: string): Promise<User> => {
   return prisma.user.update({
     where: { id: userId },
@@ -7,9 +34,34 @@ export const updateLastLogin = async (userId: string): Promise<User> => {
   });
 };
 
+export const findUserByIdWithouthPassword = async (
+  userId: string
+): Promise<Omit<
+  User,
+  | "password"
+  | "isActive"
+  | "authProvider"
+  | "providerId"
+  | "createdAt"
+  | "updatedAt"
+> | null> => {
+  return prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      avatar: true,
+      authProvider: true,
+      isVerified: true,
+      lastLoginAt: true,
+    },
+  });
+};
+
 export const updateRefreshToken = async (
   userId: string,
-  refreshToken: string
+  refreshToken: string | null
 ): Promise<Auth> => {
   return prisma.auth.update({
     where: { userId },
@@ -17,11 +69,47 @@ export const updateRefreshToken = async (
   });
 };
 
-export const findUserByVerificationCode = async (
-  verificationCode: string
+export const findUserByResetToken = async (
+  resetToken: string
 ): Promise<Auth | null> => {
   return prisma.auth.findUnique({
-    where: { verifyToken: verificationCode },
+    where: { verifyToken: resetToken },
+  });
+};
+
+export const findUserByUserID = async (
+  userId: string
+): Promise<User | null> => {
+  return prisma.user.findUnique({
+    where: { id: userId },
+  });
+};
+
+export const clearResetTokenAndExpiry = async (
+  userId: string
+): Promise<Auth> => {
+  return prisma.auth.update({
+    where: { userId },
+    data: { verifyToken: null, expiryTime: null },
+  });
+};
+
+export const updatePassword = async (
+  userId: string,
+  password: string
+): Promise<User> => {
+  return prisma.user.update({
+    where: { id: userId },
+    data: { password },
+  });
+};
+
+export const findUserByVerificationCodeAndToken = async (
+  verificationCode: string,
+  token: number
+): Promise<Auth | null> => {
+  return prisma.auth.findUnique({
+    where: { verifyToken: verificationCode, code: token },
   });
 };
 
