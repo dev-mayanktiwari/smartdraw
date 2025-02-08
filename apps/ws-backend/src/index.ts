acda0a85c34636a30b31916924d68cdf4904d2d7
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-08 22:14:14 +0530

    fix: Added ws auth

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,20 @@
 import { WebSocketServer } from "ws";
+import { wsMidddleware } from "@repo/auth";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
-  ws.on("message", (message) => {
-    console.log(`Received message => ${message}`);
-    ws.send(`You sent => ${message}`);
+wss.on("connection", (ws, request) => {
+  wsMidddleware(ws, request, (err) => {
+    if (err) {
+      console.log("Websocket authentication failed", err.message);
+      ws.close(err.code, err.message);
+      return;
+    }
+    console.log("Websocket authenticated");
+    ws.on("message", (message) => {
+      console.log("received: %s", message);
+      ws.send(`Hello, you sent -> ${message}`);
+    });
   });
 });
 
