7affd88d11b52acb991095fff70c7a5ab70427eb
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-21 12:51:49 +0530

    feat: Updated register user schema

--- packages/types/src/input/registerSchema.ts
+++ packages/types/src/input/registerSchema.ts
@@ -4,15 +4,18 @@ export const UserRegisterInput = z.object({
   name: z
     .string()
     .min(5, "Name must be atleast 5 characters long")
-    .max(50, "Name must be atmax 50 characters long")
+    .max(50, "Name must be atmost 50 characters long")
     .trim(),
-  email: z.string().email(),
+  email: z.string().email().toLowerCase(),
   password: z
     .string()
     .min(8, "Password must be 8 characters long")
     .max(30, "Password must be 30 characters long")
     .trim(),
-  username: z.string().min(5, "Username must be atleast 5 characters long"),
+  username: z
+    .string()
+    .min(5, "Username must be atleast 5 characters long")
+    .toLowerCase(),
 });
 
 export type TUserRegistrationInput = z.infer<typeof UserRegisterInput>;
