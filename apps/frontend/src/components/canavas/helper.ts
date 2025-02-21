3c10327d28ddde2588ec0ef38342eca3ec430a33
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-21 12:49:19 +0530

    feat: Added helper

---
+++ apps/frontend/src/components/canavas/helper.ts
@@ -0,0 +1,7 @@
+export function getMousePosition(event: MouseEvent, canvas: HTMLCanvasElement) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top,
+  };
+}
